0bb49bbec39812a3ebb76430e41ac0244b3035a9
Momchil Atanasov <momchil.dev@gmail.com>
2024-09-08 11:30:18 +0300

    Align with v0.20.0 of lacking (#2)

--- cmd/game/app_js.go
+++ cmd/game/app_js.go
@@ -1,13 +1,13 @@
-//go:build !js
+//go:build js
 
 package main
 
 import (
 	"fmt"
 
-	nativeapp "github.com/mokiat/lacking-native/app"
-	nativegame "github.com/mokiat/lacking-native/game"
-	nativeui "github.com/mokiat/lacking-native/ui"
+	jsapp "github.com/mokiat/lacking-js/app"
+	jsgame "github.com/mokiat/lacking-js/game"
+	jsui "github.com/mokiat/lacking-js/ui"
 	gameui "github.com/mokiat/lacking-template/internal/ui"
 	"github.com/mokiat/lacking-template/resources"
 	"github.com/mokiat/lacking/app"
@@ -18,7 +18,7 @@ import (
 )
 
 func runApplication() error {
-	registryStorage, err := asset.NewFSStorage("./assets")
+	registryStorage, err := asset.NewWebStorage(".")
 	if err != nil {
 		return fmt.Errorf("failed to initialize storage: %w", err)
 	}
@@ -32,18 +32,14 @@ func runApplication() error {
 
 	locator := ui.WrappedLocator(resource.NewFSLocator(resources.UI))
 
-	gameController := game.NewController(registry, nativegame.NewShaderCollection(), nativegame.NewShaderBuilder())
-	uiController := ui.NewController(locator, nativeui.NewShaderCollection(), func(w *ui.Window) {
+	gameController := game.NewController(registry, jsgame.NewShaderCollection(), jsgame.NewShaderBuilder())
+	uiController := ui.NewController(locator, jsui.NewShaderCollection(), func(w *ui.Window) {
 		gameui.BootstrapApplication(w, gameController)
 	})
 
-	cfg := nativeapp.NewConfig("Game", 1280, 800)
+	cfg := jsapp.NewConfig("screen")
+	cfg.AddGLExtension("EXT_color_buffer_float")
 	cfg.SetFullscreen(false)
-	cfg.SetMaximized(false)
-	cfg.SetMinSize(1024, 576)
-	cfg.SetVSync(true)
-	cfg.SetIcon("ui/images/icon.png")
-	cfg.SetLocator(locator)
 	cfg.SetAudioEnabled(false)
-	return nativeapp.Run(cfg, app.NewLayeredController(gameController, uiController))
+	return jsapp.Run(cfg, app.NewLayeredController(gameController, uiController))
 }
